f72a86c09772c0b7f7829fe3ef0d3dc03d28007f
Krithik Singh <krithiksingh@gmail.com>
2023-04-29 13:39:54 -0400

    lab completed!

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 // Write your code here!
-const testVar = "hi";
-document.querySelector("main#main").remove();
-const newHeader = document.createElement("h1");
-newHeader.id = "victory"
-newHeader.innerHTML = "Flatiron School is the champion!";
-newHeader.className = "victory";
+main.remove();
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = 'Krithik is the champion';
